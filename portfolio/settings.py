1ccb76c8c14ce0f2ce6eb89f55dcea2a7e35a1f5
Vampire-code-rgb <adigondu@gmail.com>
2025-06-18 01:56:11 +0530

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--*vycv@%&a)*pk()c^7kz*%8q*=g-&o-4vu)(d@wb@*d7s2m5z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
